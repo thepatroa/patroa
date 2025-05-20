c470495b41163d3bc105911ab8cc2ee9f0e47308
Atilio <thehacktour@gmail.com>
2025-05-20 08:38:01 +0100

    fix: 🦁 improve run.py setup basic configuration

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-import uvicorn
+from fastapi import FastAPI
+from src.routes import deploy_routes
+from src.database import Base, engine
 
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(deploy_routes.router)
