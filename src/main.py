fcb049d13710f28b793aa5921e7675a8f3a2eff1
Atilio <thehacktour@gmail.com>
2025-05-20 08:50:22 +0100

    refactor: 🐳 delete run.py and put all setup inside main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from src.routes import deploy_routes
 from src.database import Base, engine
 
+import uvicorn
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
-
 app.include_router(deploy_routes.router)
+
+if __name__ == "__main__":
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
