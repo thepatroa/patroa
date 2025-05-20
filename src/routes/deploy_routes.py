c5c92ca89e5de26b1dad9a3e84e7810408de35cd
Atilio <thehacktour@gmail.com>
2025-05-20 08:33:48 +0100

    feat: 🦅 add routes

---
+++ src/routes/deploy_routes.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.database import SessionLocal
+from src.controllers import deploy_controller
+from src.schemas import deploy_schema
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/deploys", response_model=deploy_schema.DeployOut)
+def create(deploy: deploy_schema.DeployCreate, db: Session = Depends(get_db)):
+    return deploy_controller.create_deploy(db, deploy)
+
+@router.get("/deploys", response_model=list[deploy_schema.DeployOut])
+def list_all(db: Session = Depends(get_db)):
+    return deploy_controller.list_deploys(db)
