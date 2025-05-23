88ee98fa0e8106e1156976058d167921fbbb8d68
Atilio <thehacktour@gmail.com>
2025-05-23 18:24:40 +0100

    refactor: 🐳 add cors setup

--- api/src/main.py
+++ api/src/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from src.routes import deploy_routes
 from src.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 app.include_router(deploy_routes.router)
