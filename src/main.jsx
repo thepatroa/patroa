f68088f21a9e4b39c2d73b300feb0770c342bd87
Atilio <thehacktour@gmail.com>
2025-06-02 09:27:39 +0100

    refactor: 🐳 start again

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
-import { SidebarProvider } from './context/sidebarContext.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <SidebarProvider>
-    <App />
-  </SidebarProvider>
-)
